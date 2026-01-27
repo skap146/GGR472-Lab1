7122609189853909d5a010619ab819c964ad4d21
shawn kapcan <skapcan52@gmail.com>
2026-01-27 14:13:44 -0500

    finalizing site ..

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// loading the restaurant data (for user input module)
+let restaurantNames = [];
+
+fetch("data/restaurant_data.geojson")
+    .then(res => res.json())
+    .then(data => {
+        restaurantNames = data.features.map(feature => feature.properties.name.toLowerCase());
+    });
 
 // this file handles all button interactions for the website.
 
@@ -25,6 +33,7 @@ function button_event_listener(btn_id, link, newtab=false)
 button_event_listener('home', 'index.html');
 button_event_listener('behind_the_scenes', 'behind_the_scenes.html');
 button_event_listener('external_links', 'external_links.html');
+button_event_listener('user_input', 'user_input.html');
 button_event_listener('see_geojson', 'data/restaurant_data.geojson', true)
 button_event_listener('trip_advisor',
     'https://www.tripadvisor.ca/FindRestaurants?geo=155019&sort=POPULARITY&establishmentTypes=10591&broadened=false',
@@ -33,3 +42,37 @@ button_event_listener('opentable', 'https://www.opentable.ca/s?lang=en-ca&covers
     true)
 button_event_listener('reddit', 'https://www.reddit.com/r/askTO/comments/1q1r3n1/what_restaurants_in_toronto_made_you_say_holy/',
     true)
+
+// user submission event listener (special functionality)
+function checkRestaurant()
+{
+    // parse user input, case insensitive
+    const raw_input = document.querySelector('input').value
+    const user_input = raw_input.trim().toLowerCase();
+
+    // find result element, where text will be later appended to based on user input
+    const resultDiv = document.getElementById("res");
+
+    // prompt user to type something if they input a empty string
+    if (!user_input)
+    {
+
+        resultDiv.textContent = "Sorry, please try again and enter a valid restaurant name."
+    }
+
+    console.log(restaurantNames);
+    console.log(user_input);
+
+    // match
+    const match = restaurantNames.includes(user_input);
+
+    // print an alert to be viewed by user
+    if (match)
+    {
+        resultDiv.textContent = "Nice! Your restaurant " + raw_input + " is one of Toronto's Top 10!!!"
+    }
+    else
+    {
+        resultDiv.textContent = "Your restaurant " + raw_input + " is not currently one of Toronto's Top 10! Hidden gem perhaps?"
+    }
+}
